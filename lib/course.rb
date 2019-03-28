e1bc7d8003d7fbc3536afdd3d23d0bb60cc59fae
kyle smith <kylemclellan@gmail.com>
2019-03-28 18:35:58 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,18 @@
-
-
+class Course 
+  @@all = []
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.reset_all 
+    @@all.clear 
+  end 
+  
+  attr_accessor :title, :schedule, :description
+  
+  def initialize
+    @@all << self 
+  end 
+  
+end 
